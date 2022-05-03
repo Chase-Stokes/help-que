fefd178c3a482872bc6509b05e4258fd1a6e06f4
chase-stokes <chasehstokes@gmail.com>
2022-05-03 09:30:53 -0700

    add redux, and firebase

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -3,18 +3,21 @@ import NewTicketForm from './NewTicketForm';
 import TicketList from './TicketList';
 import EditTicketForm from './EditTicketForm';
 import TicketDetail from './TicketDetail';
+import { connect } from 'react-redux';
+import PropTypes from "prop-types";
+import * as a from './../actions';
 
 class TicketControl extends React.Component {
 
     constructor(props) {
         super(props);
+        console.log(props + " ticketControl constructor props")
         this.state = {
-            formVisibleOnPage: false,
-            mainTicketList: [],
+            // formVisibleOnPage: false, //handled with redux now
             selectedTicket: null,
             editing: false
         };
-        this.handleClick = this.handleClick.bind(this);
+        // this.handleClick = this.handleClick.bind(this);
     }
 
     //It's common practice to prefix the name of an event handler function with handle
@@ -23,48 +26,92 @@ class TicketControl extends React.Component {
         this.setState({editing: true});
     }
     handleEditingTicketInList = (ticketToEdit) => {
-        const editedMainTicketList = this.state.mainTicketList
-        .filter(ticket => ticket.id !== this.state.selectedTicket.id)
-        .concat(ticketToEdit);
+        const { dispatch } = this.props;
+        const action = a.addTicket(ticketToEdit);
+        dispatch(action);
         this.setState({
-            mainTicketList: editedMainTicketList,
             editing: false,
             selectedTicket: null
         });
     }
-
+    
+    // handleEditingTicketInList = (ticketToEdit) => {
+    //     const { dispatch } = this.props;
+    //     const { id, names, location, issue } = ticketToEdit;       
+    //     const action = {
+    //         type: 'ADD_TICKET',
+    //         id: id,
+    //         names: names,
+    //         location: location,
+    //         issue: issue,
+    //     }
+    //     dispatch(action);
+    //     this.setState({
+    //         editing: false,
+    //         selectedTicket: null
+    //     });
+    // }
     handleDeletingTicket = (id) => {
-        const newMainTicketList = this.state.mainTicketList.filter(ticket => ticket.id !== id);
-        this.setState({
-            mainTicketList: newMainTicketList,
-            selectedTicket: null
-        });
+        const { dispatch } = this.props;
+        const action = a.deleteTicket(id);
+        dispatch(action);
+        this.setState({selectedTicket: null});
     }
+    // handleDeletingTicket = (id) => {
+    //     const { dispatch } = this.props;
+    //     const deleteTicket = id => ({
+    //         type: 'DELETE_TICKET',
+    //         id
+    //     })
+    //     const action = deleteTicket(id)
+    //     // const action = {
+    //     //     type: 'DELETE_TICKET',
+    //     //     id: id
+    //     // }
+    //     dispatch(action);
+    //     this.setState({selectedTicket: null});
+    // }
 
     handleChangingSelectedTicket = (id) => {
-        const selectedTicket = this.state.mainTicketList.filter(ticket => ticket.id === id)[0];
+        const selectedTicket = this.props.mainTicketList[id];
         this.setState({selectedTicket: selectedTicket})
     }
 
     handleAddingNewTicketToList = (newTicket) => {
-        const newMainTicketList = this.state.mainTicketList.concat(newTicket);
-        this.setState({
-            mainTicketList: newMainTicketList,
-            formVisibleOnPage: false 
-        });
+        const { dispatch } = this.props;
+        const action = a.addTicket(newTicket);
+        dispatch(action);
+        const action2 = a.toggleForm();
+        dispatch(action2);
     }
+    // handleAddingNewTicketToList = (newTicket) => {
+    //     const { dispatch } = this.props;
+    //     const { id, names, location, issue } = newTicket;
+    //     const action = {
+    //         type: 'ADD_TICKET',
+    //         id: id,
+    //         names: names,
+    //         location: location,
+    //         issue: issue,
+    //     }
+    //     dispatch(action);
+    //     // this.setState({formVisibleOnPage: false }); //handled with redux now
+    // }
 
     handleClick = () => {
         if (this.state.selectedTicket != null) {
             this.setState({
-            formVisibleOnPage: false,
+            // formVisibleOnPage: false, //handled with redux now
             selectedTicket: null,
             editing: false
             });
         } else {
-            this.setState(prevState => ({
-            formVisibleOnPage: !prevState.formVisibleOnPage,
-            }));
+            const { dispatch } = this.props;
+            const action = a.toggleForm();
+            dispatch(action);
+            // this.setState(prevState => ({
+            // formVisibleOnPage: !prevState.formVisibleOnPage,
+            // })); //handled with redux now
         }
     }
     // prefix props with "on" -- as in onNewTicketCreation
@@ -83,14 +130,14 @@ class TicketControl extends React.Component {
 
             // While our TicketDetail component only takes placeholder data, we will eventually be passing the value of selectedTicket as a prop.
         }
-        else if (this.state.formVisibleOnPage) {
+        else if (this.props.formVisibleOnPage) { //changed this.state to this.props 
 
         // This conditional needs to be updated to "else if."
 
         currentlyVisibleState = <NewTicketForm onNewTicketCreation={this.handleAddingNewTicketToList}  />;
         buttonText = "Return to Ticket List";
         } else {
-        currentlyVisibleState = <TicketList ticketList={this.state.mainTicketList} onTicketSelection={this.handleChangingSelectedTicket} />;
+        currentlyVisibleState = <TicketList ticketList={this.props.mainTicketList} onTicketSelection={this.handleChangingSelectedTicket} />;
 
         // Because a user will actually be clicking on the ticket in the Ticket component, we will need to pass our new handleChangingSelectedTicket method as a prop.
 
@@ -106,4 +153,17 @@ class TicketControl extends React.Component {
 
 }
 
+TicketControl.propTypes = {
+    mainTicketList: PropTypes.object,
+    formVisibleOnPage: PropTypes.bool
+};
+
+const mapStateToProps = state => {
+    return {
+        // Key-value pairs of state to be mapped from Redux to React component go here.
+        mainTicketList: state.mainTicketList,
+        formVisibleOnPage: state.formVisibleOnPage
+    }
+}
+TicketControl = connect(mapStateToProps)(TicketControl);
 export default TicketControl;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './components/index.css';
 import App from './components/App';
+import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+// import reducer from './reducers/ticket-list-reducer';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/index';
+import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
+import { createFirestoreInstance } from 'redux-firestore';
+import firebase from "./firebase";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// const store = createStore(reducer);
+const store = createStore(rootReducer);
+
+store.subscribe(() =>
+  console.log(store.getState() + " indexJS store.subscribe")
+)
+
+const rrfProps = {
+  firebase,
+  config: {
+        userProfile: "users"
+    },
+  dispatch: store.dispatch,
+  createFirestoreInstance
+}
+
+ReactDOM.render(
+    <Provider store={store}>
+      <ReactReduxFirebaseProvider {...rrfProps}>
+          <App />
+      </ReactReduxFirebaseProvider>
+    </Provider>,
+    document.getElementById('root')
 );
+
+serviceWorker.unregister();
 
